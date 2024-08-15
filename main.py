71cca81488013858ac5dfa27876d28273edef012
sonyakun <90973770+sonyakun@users.noreply.github.com>
2024-08-16 00:44:10 +0900

    ログ改善、configでipとportを指定できるように、起動にかかった時間をミリ秒ではなく秒に

--- main.py
+++ main.py
@@ -2,20 +2,28 @@ import asyncio
 import time
 
 from app import Server, logger
+from app.config import Config
 
 HOMURAMC = "2024.08.15β"
 
 startTime = time.time()
 log = logger.get_module_logger("HomuraMC")
 
-log.info(f"""HomuraMC v{HOMURAMC} is starting...""")
+log.info(f"""Starting HomuraMC v{HOMURAMC}...""")
+
+log.info("Loading properties from homura.yml")
+config = Config().config
+log.info("HomuraMC configuration file loaded successfully")
 
 
 async def main():
-    server = await asyncio.start_server(Server.serverLoop, "0.0.0.0", 25565)
+    server = await asyncio.start_server(
+        Server.serverLoop, config.server.ip, config.server.port
+    )
     endTime = time.time()
-    during = endTime - startTime
-    log.info(f"loaded!({during}ms)")
+    during = (endTime - startTime) * 1000
+    during_s = during / 1000
+    log.info(f"Done ({during_s}s)! listening on {config.server.ip}:{config.server.port}...")
     await server.serve_forever()
 
 
